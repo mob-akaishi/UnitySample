d41eba48fbe9e4ce056a6f02135dde7289525c8a
赤石　洋輔 <akaishi@mobcast.jp>
2015-04-09 15:20:28 +0900

    プロジェクト作成

---
+++ project/Assets/src/Sample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log("start");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
